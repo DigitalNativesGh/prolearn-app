f5e4b6ae447fde89cb5dd2852554874204e80d66
digitalnativesltd <henrybaah2016@gmail.com>
2024-08-28 12:52:06 -0700

    update

--- app/src/main/java/com/example/prolearn/QuizResult.kt
+++ app/src/main/java/com/example/prolearn/QuizResult.kt
@@ -1,17 +1,50 @@
- package com.example.prolearn
+package com.example.prolearn
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.prolearn.ui.theme.ProlearnTheme
+
 
 class QuizResult : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +52,10 @@ class QuizResult : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProlearnTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    QuizResultScreen()
                 }
             }
         }
@@ -31,17 +63,149 @@ class QuizResult : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun QuizResultScreen() {
+    Scaffold(
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProlearnTheme {
-        Greeting("Android")
+    ) { paddingValues ->
+        QuizResults(Modifier.padding(paddingValues))
     }
 }
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QuizResults(modifier: Modifier = Modifier) {
+
+    var searchText by remember { mutableStateOf("") }
+    val scrollState = rememberScrollState()
+
+    Scaffold(
+
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .fillMaxHeight()
+                .verticalScroll(scrollState),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+
+            Box (modifier = Modifier.padding(top=100.dp, bottom = 60.dp)){
+                QuizResultContent()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+@Composable
+fun QuizResultContent() {
+    Column(
+
+    ) {
+        Row (
+
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+
+        ){
+            Column (
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+
+                Image(
+                    painter = painterResource(id = R.drawable.success_person),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(250.dp)
+
+                )
+                Text(
+                    text = "Congratulation",
+                    modifier = Modifier
+                        .padding(start = 14.dp, end = 6.dp, top = 30.dp),
+                    color = colorResource(id = R.color.blue),
+                    fontSize = 23.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Box(
+                    modifier = Modifier
+                        .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 8.dp)
+                        .dashedBorder(
+                            color = colorResource(id = R.color.green),
+                            strokeWidth = 2.dp,
+                            dashLength = 8.dp,
+                            gapLength = 4.dp,
+                            cornerRadius = 3.dp
+                        )
+                        .clip(RoundedCornerShape(6.dp))
+                        .background(colorResource(id = R.color.white))
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = "Return To Courses",
+                            fontSize = 18.sp,
+                            color = colorResource(id = R.color.green),
+                            fontWeight = FontWeight.SemiBold,
+
+                            )
+
+                    }
+                }
+
+            }
+        }
+
+        Button(
+            onClick = {
+
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.blue),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 180.dp, start = 20.dp, end = 20.dp)
+                .height(55.dp)
+        ) {
+            Text("Check Your Points", fontSize = 18.sp)
+        }
+
+        Button(
+            onClick = {
+
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.white),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 0.dp, start = 20.dp, end = 20.dp)
+                .height(55.dp)
+        ) {
+            Text("Go To My Courses",
+                fontSize = 18.sp,
+                color = colorResource(id = R.color.blue)
+            )
+        }
+
+    }
+
+}
+
+
+
